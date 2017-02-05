d78312fec80eed864809402b8c4cf627f8a1a3cc
Dirk Schürjohann <ds@decaf.de>
2017-02-05 11:59:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -o errexit -o nounset
 
-JEKYLL_GITHUB_TOKEN=885cd3e7960bb4f314c1330377207a6789493e2b bundle exec jekyll build
+JEKYLL_GITHUB_TOKEN=$JEKYLL_GITHUB_TOKEN bundle exec jekyll build
