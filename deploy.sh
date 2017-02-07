3f676a2bd21edf134ff86ad3a4862df42f953190
schuer <ds@decaf.de>
2017-02-07 11:40:13 +0100

    update travis config and scripts
    * split up build and deploy into separate scripts
    * run deploy after build success only

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+# deploy
+# on cron only
+if [[ $TRAVIS_EVENT_TYPE == "cron" ]] ; then
+
+    # define git credentials for travis
+    git config --global user.name "Travis CI"
+    git config --global user.email "travis@travis-ci.org"
+
+    # push without commit
+    # see https://github.com/FriendsOfREDAXO/friendsofredaxo.github.io/issues/29#issuecomment-277445022
+    # redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed
+    git push --force --quiet "https://${GITHUB_TOKEN}@github.com/FriendsOfREDAXO/friendsofredaxo.github.io.git" HEAD^:master  > /dev/null 2>&1
+    git push         --quiet "https://${GITHUB_TOKEN}@github.com/FriendsOfREDAXO/friendsofredaxo.github.io.git" master        > /dev/null 2>&1
+fi
