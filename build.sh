aef3f8f384776215924646a93bb8c4c45f731888
schuer <ds@decaf.de>
2017-02-07 11:26:13 +0100

    update build script
    * use new token
    * try silent force push triggered by travis cron

--- build.sh
+++ build.sh
@@ -2,4 +2,20 @@
 
 set -o errexit -o nounset
 
-JEKYLL_GITHUB_TOKEN=$JEKYLL_GITHUB_TOKEN bundle exec jekyll build
+# build
+JEKYLL_GITHUB_TOKEN=$GITHUB_TOKEN bundle exec jekyll build
+
+# deploy
+# on cron only
+if [[ $TRAVIS_EVENT_TYPE == "cron" ]] ; then
+
+    # define git credentials for travis
+    git config --global user.name "Travis CI"
+    git config --global user.email "travis@travis-ci.org"
+
+    # push without commit
+    # see https://github.com/FriendsOfREDAXO/friendsofredaxo.github.io/issues/29#issuecomment-277445022
+    # redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed
+    git push --force --quiet "https://${GITHUB_TOKEN}@github.com/FriendsOfREDAXO/friendsofredaxo.github.io.git" HEAD^:master  > /dev/null 2>&1
+    git push         --quiet "https://${GITHUB_TOKEN}@github.com/FriendsOfREDAXO/friendsofredaxo.github.io.git" master        > /dev/null 2>&1
+fi
