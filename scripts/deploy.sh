4e390f771196008eaf6cf10993a9d29d8d21091d
schuer <ds@decaf.de>
2017-02-15 16:01:09 +0100

    scripts: check for token available + make exec

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,18 @@
 
 set -o errexit -o nounset
 
-# Requesting a page build
-# Blog: https://developer.github.com/changes/2016-07-06-github-pages-preview-api/
-# Docs: https://developer.github.com/v3/repos/pages/#request-a-page-build
-curl "https://api.github.com/repos/FriendsOfREDAXO/friendsofredaxo.github.io/pages/builds" \
-    -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
-    -H "Accept: application/vnd.github.mister-fantastic-preview"
+if [ -n "${GITHUB_TOKEN+1}" ] # is github token declared?
+then
+
+    # Requesting a page build
+    # Blog: https://developer.github.com/changes/2016-07-06-github-pages-preview-api/
+    # Docs: https://developer.github.com/v3/repos/pages/#request-a-page-build
+    curl "https://api.github.com/repos/FriendsOfREDAXO/friendsofredaxo.github.io/pages/builds" \
+        -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
+        -H "Accept: application/vnd.github.mister-fantastic-preview"
+
+    echo "Requested page build! <3"
+
+else
+    echo "Did not request page build."
+fi
